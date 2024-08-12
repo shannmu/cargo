2d77e710923511495aa094f93887a50d01abcf69
shannmu <shanmu1901@gmail.com>
2024-08-12 17:24:01 +0800

    feat: switch hand-written completion files to instead register the completer

--- src/bin/cargo/commands/mod.rs
+++ src/bin/cargo/commands/mod.rs
@@ -7,6 +7,7 @@ pub fn builtin() -> Vec<Command> {
         build::cli(),
         check::cli(),
         clean::cli(),
+        complete::cli(),
         config::cli(),
         doc::cli(),
         fetch::cli(),
@@ -52,6 +53,7 @@ pub fn builtin_exec(cmd: &str) -> Option<Exec> {
         "build" => build::exec,
         "check" => check::exec,
         "clean" => clean::exec,
+        "complete" => complete::exec,
         "config" => config::exec,
         "doc" => doc::exec,
         "fetch" => fetch::exec,
@@ -95,6 +97,7 @@ pub mod bench;
 pub mod build;
 pub mod check;
 pub mod clean;
+pub mod complete;
 pub mod config;
 pub mod doc;
 pub mod fetch;
